c08c0a4d99d8d274cf49b226002f0c0b6565f314
Sodiq Akinjobi <sodiq.akinjobi@gmail.com>
2024-05-10 10:57:47 +0000

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-  return "Hello world!"
+  return "When will this eye emoji end and you will use your words?"
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=8080)  # Replace 8080 with your desired port
